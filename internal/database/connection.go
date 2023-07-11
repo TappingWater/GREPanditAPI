b6b53384e840fb4ea82618f210eeedfc3857d5b5
TappingWater <chanakaperera301@gmail.com>
2023-07-12 00:00:37 +0530

    first commit

--- internal/database/connection.go
+++ internal/database/connection.go
@@ -6,65 +6,41 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/secretsmanager"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 	"github.com/jackc/pgx/v4/pgxpool"
-	"github.com/joho/godotenv"
 )
 
 type DBSecrets struct {
-	Host     string `json:"host"`
 	Username string `json:"username"`
 	Password string `json:"password"`
-	DBName   string `json:"dbname"`
-	Port     string `json:"port"`
-	SSLMode  string `json:"sslmode"`
-}
-
-func init() {
-	// Load environment variables from .env file during development
-	appEnv := os.Getenv("APP_ENV")
-	if appEnv == "" {
-		appEnv = "dev"
-	}
-	// Load environment variables from the appropriate .env file
-	envFile := fmt.Sprintf(".env.%s", appEnv)
-	if err := godotenv.Load(envFile); err != nil {
-		fmt.Printf("No %s file found\n", envFile)
-	}
 }
 
 func getDBCredentials() (DBSecrets, error) {
-	if os.Getenv("APP_ENV") == "prod" {
-		sess := session.Must(session.NewSession(&aws.Config{
-			Region: aws.String("us-east-1"),
-		}))
-		svc := secretsmanager.New(sess)
-		secretName := "rds!db-d7a1a557-6da2-4b05-8b7d-65ea491f3bd7" // Replace with your secret name
-		input := &secretsmanager.GetSecretValueInput{
-			SecretId: &secretName,
-		}
-		result, err := svc.GetSecretValue(input)
-		if err != nil {
-			return DBSecrets{}, err
-		}
-		var secretData DBSecrets
-		err = json.Unmarshal([]byte(*result.SecretString), &secretData)
-		if err != nil {
-			return DBSecrets{}, err
-		}
-		return secretData, nil
-	} else {
-		return DBSecrets{
-			Host:     os.Getenv("DB_HOST"),
-			Username: os.Getenv("DB_USER"),
-			Password: os.Getenv("DB_PASSWORD"),
-			DBName:   os.Getenv("DB_NAME"),
-			Port:     os.Getenv("DB_PORT"),
-			SSLMode:  os.Getenv("DB_SSLMODE"),
-		}, nil
+	if os.Getenv("APP_ENV") == "dev" {
+		return DBSecrets{os.Getenv("DB_HOST"), os.Getenv("DB_PASSWORD")}, nil
 	}
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(os.Getenv("AWS_REGION")))
+	if err != nil {
+		return DBSecrets{}, fmt.Errorf("unable to load SDK config, %v", err)
+	}
+	// Create Secrets Manager client
+	svc := secretsmanager.NewFromConfig(cfg)
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(os.Getenv("SECRET_NAME")),
+		VersionStage: aws.String("AWSCURRENT"),
+	}
+	result, err := svc.GetSecretValue(context.TODO(), input)
+	if err != nil {
+		return DBSecrets{}, err
+	}
+	var secretData DBSecrets
+	err = json.Unmarshal([]byte(*result.SecretString), &secretData)
+	if err != nil {
+		return DBSecrets{}, err
+	}
+	return secretData, nil
 }
 
 /**
@@ -78,12 +54,12 @@ func ConnectDB() (*pgxpool.Pool, error) {
 	}
 	connStr := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=UTC",
-		secrets.Host,
+		os.Getenv("DB_HOST"),
 		secrets.Username,
 		secrets.Password,
-		secrets.DBName,
-		secrets.Port,
-		secrets.SSLMode,
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"),
 	)
 	dbpool, err := pgxpool.Connect(context.Background(), connStr)
 	if err != nil {
